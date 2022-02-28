532a9a24b41272349714f71e9c040c697d8af943
Josh Burrows <jburrowsDev@gmail.com>
2022-02-27 22:21:37 -0600

    a

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Login from "./login";
+import SignUp from "./Signup";
+
+function App() {
+  return (
+    <>
+      <div>Hello World</div>
+      <Login />
+      <SignUp />
+    </>
+  );
+}
+export default App;
